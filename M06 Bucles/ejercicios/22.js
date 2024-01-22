0f98b7f171a0bbf6b405a55329902db7e495d022
ramongonzalez101 <ramongonzalez101@gmail.com>
2024-01-22 17:54:21 +0100

    ejercicio js221

--- M06 Bucles/ejercicios/22.js
+++ M06 Bucles/ejercicios/22.js
@@ -3,6 +3,75 @@ function diasEnMes(mes) {
   // Determine cúantos días tiene el mes correspondiente a ese número.
   // PISTA: Usa un bucle do-while para validar que el mes ingresado sea válido.
   // Tu código:
+
+var i = 0;
+  // do {
+  //       i++
+  //   console.log ('1. pasada:' + i);
+
+  //   if (mes !== i){
+  //     return 0;
+  //   }
+
+  // } while (i < 12);
+  // Nota: Lo hice sin do while
+
+  switch (mes) {
+    case 1:
+      return 31;
+      break;
+      
+    case 2:
+      return 28;
+      break;
+  
+    case 3:
+    return 31;
+    break;
+    
+    case 4:
+    return 30;
+    break; 
+
+    case 5:
+    return 31;
+    break; 
+
+    case 6:
+    return 30;
+    break; 
+
+    case 7:
+    return 31;
+    break; 
+
+    case 8:
+    return 31;
+    break; 
+
+    case 9:
+    return 30;
+    break; 
+
+    case 10:
+    return 31;
+    break; 
+
+    case 11:
+    return 30;
+    break; 
+
+    case 12:
+    return 31;
+    break; 
+
+    default:
+      return 0;
+      break;
+  }
 }
+// }
+
+console.log (diasEnMes(0));
 
 module.exports = diasEnMes;
--- M06 Bucles/ejercicios/23.js
+++ M06 Bucles/ejercicios/23.js
@@ -3,6 +3,22 @@ function doWhile(num) {
   // Retorna el valor final.
   // PISTA: Utiliza el bucle do-while.
   // Tu código:
+var i = 0;
+  do {
+    i++
+    console.log ('pasada: ' + i);
+    num += 5;
+
+    console.log ('sumas: ' + num);
+    if (i === 8){
+      return num;
+    }
+
+  } while (i <= 7);
+
 }
 
 module.exports = doWhile;
+
+console.log (doWhile(80));
+
